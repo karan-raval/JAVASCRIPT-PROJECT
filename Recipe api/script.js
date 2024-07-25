d0d24f2c1f18eb19ceac4900bb0cdc3e690d7fe8
Raval Karan <157629752+karan-raval@users.noreply.github.com>
2024-07-25 09:57:50 +0530

    Recipe Api

--- Recipe api/script.js
+++ Recipe api/script.js
@@ -4,18 +4,35 @@ const mealDetailsContent = document.querySelector(".meal-details");
 searchBtn.addEventListener("click", getMealList);
 mealList.addEventListener("click", getMealRecipe);
 
-
 function getMealList() {
   let searchInputTxt = document.getElementById("search-input").value;
-  fetch(`https://api.edamam.com/search?q=${searchInputTxt}&app_id=a56a38ea&app_key=5e4279147e5b09e1e9470abc0fba0401`)
-    .then((response) => response.json())
-    .then((data) => {
-      // console.log(data);
-      let html = "";
-      if (data.hits) {
-        data.hits.forEach((meal) => {
-          html += `
-                    <div class = "meal-item">
+  // console.log(searchInputTxt);
+  if (searchInputTxt == "") {
+    Swal.fire({
+      position: "top-end",
+      icon: "error",
+      title: "Please Enter Recipe Name",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  } else {
+    fetch(
+      `https://api.edamam.com/search?q=${searchInputTxt}&app_id=a56a38ea&app_key=5e4279147e5b09e1e9470abc0fba0401`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        let html = "";
+        if (data.more == false) {
+          Swal.fire({
+            position: "top-end",
+            icon: "error",
+            title: "Please Re-Enter Recipe Name",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }else if (data.hits) {
+          data.hits.forEach((meal) => {
+            html += `<div class = "meal-item">
                         <div class = "meal-img">
                             <img src = "${meal.recipe.image}" alt = "food">
                         </div>
@@ -24,39 +41,28 @@ function getMealList() {
                             <a href = "#" class = "recipe-btn">Get Recipe</a>
                         </div>
                     </div>
-            </div>
-
-                `;
-        });
-        mealList.classList.remove("notFound");
-      } else {
-        html = "Sorry, we didn't find any meal!";
-        mealList.classList.add("notFound");
-      }
-
-      mealList.innerHTML = html;
-    });
+            </div>`;
+          });
+          mealList.innerHTML = html;
+          mealList.classList.remove("notFound");
+        } 
+      });
+  }
 }
 
 function getMealRecipe(e) {
   e.preventDefault();
-  // let mealItem = e.target.parentElement.children[0].getAttribute("value");
-  let mealItem =e.target.parentElement.getElementsByTagName('h3')[0].innerHTML;
-  // console.log(mealItem);
-  // if (e.target.classList.contains("recipe-btn")) {
-    fetch(
-      `https://api.edamam.com/api/recipes/v2?type=public&q=${mealItem}&sapp_id=a56a38ea&app_key=5e4279147e5b09e1e9470abc0fba0401	`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        mealRecipeModal(data);
-      });
-    }
-    // }
-    
-    function mealRecipeModal(meal) 
-  {
+  let mealItem = e.target.parentElement.getElementsByTagName("h3")[0].innerHTML;
+  fetch(
+    `https://api.edamam.com/api/recipes/v2?type=public&q=${mealItem}&sapp_id=a56a38ea&app_key=5e4279147e5b09e1e9470abc0fba0401`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      mealRecipeModal(data);
+    });
+}
+
+function mealRecipeModal(meal) {
   let html = `
   <button type = "button" class = "btn recipe-close-btn" onclick=rc() >
           <i class = "fas fa-times"></i>
@@ -66,7 +72,8 @@ function getMealRecipe(e) {
     <p class = "recipe-category">Calories=${meal.hits[0].recipe.calories}</p>
 <div class = "recipe-instruct">
     <h3>Instructions:</h3>
-    <p><span>${meal.hits[0].recipe.ingredientLines.map((ing) => `<li class="ing">${ing}</li>`)
+    <p><span>${meal.hits[0].recipe.ingredientLines
+      .map((ing) => `<li class="ing">${ing}</li>`)
       .join("")}</span></p>
 </div>
 <div class = "recipe-meal-img">
@@ -81,11 +88,37 @@ function getMealRecipe(e) {
   mealDetailsContent.classList.add("showRecipe");
 }
 
-
-function rc()
-{
+function rc() {
   mealDetailsContent.classList.remove("showRecipe");
-
 }
 
-{/* <div class = "meal-item" data-id = "${meal.idMeal}"></div> */}
+
+
+function bydefault()
+{
+  fetch(
+    `https://api.edamam.com/search?q=salad&app_id=a56a38ea&app_key=5e4279147e5b09e1e9470abc0fba0401`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      let html = "";
+      if (data.hits) {
+        data.hits.forEach((meal) => {
+          html += `<div class = "meal-item">
+                      <div class = "meal-img">
+                          <img src = "${meal.recipe.image}" alt = "food">
+                      </div>
+                      <div class = "meal-name">
+                          <h3 value=${meal.recipe.label}>${meal.recipe.label}</h3>
+                          <a href = "#" class = "recipe-btn">Get Recipe</a>
+                      </div>
+                  </div>
+          </div>`;
+        })
+        mealList.innerHTML = html;
+        mealList.classList.remove("notFound");
+}
+})
+}
+
+bydefault()
